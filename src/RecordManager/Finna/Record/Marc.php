54ec322f0b9e7c85235a52c4c1d38c3fd1003391
rajaro <jaro.ravila@gmail.com>
2021-12-08 14:24:53 +0200

    [ALLI-7189] Index audience characteristics to topics (#6)

--- src/RecordManager/Finna/Record/Marc.php
+++ src/RecordManager/Finna/Record/Marc.php
@@ -1932,7 +1932,8 @@ class Marc extends \RecordManager\Base\Record\Marc
                 [self::GET_NORMAL, '648', ['x' => 1]],
                 [self::GET_NORMAL, '650', ['a' => 1, 'x' => 1]],
                 [self::GET_NORMAL, '651', ['x' => 1]],
-                [self::GET_NORMAL, '655', ['x' => 1]]
+                [self::GET_NORMAL, '655', ['x' => 1]],
+                [self::GET_NORMAL, '385', ['a' => 1]]
             ],
             false,
             true,
@@ -1954,7 +1955,8 @@ class Marc extends \RecordManager\Base\Record\Marc
     {
         $result = array_merge(
             parent::getTopics(),
-            $this->get653WithSecondInd([' ', '0', '1', '2', '3'])
+            $this->get653WithSecondInd([' ', '0', '1', '2', '3']),
+            $this->getFieldsSubfields([[self::GET_NORMAL, '385', ['a' => 1]]])
         );
         return $result;
     }
